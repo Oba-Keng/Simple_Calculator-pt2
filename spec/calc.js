5369df41a998c3c109489edafd24b11cf8a6f2f8
Obakeng <obeezus@gmail.com>
2019-06-28 12:13:09 +0200

    second commit

--- spec/calc.js
+++ spec/calc.js
@@ -1,63 +1,44 @@
-module.exports = class Calculator{
-
-constructor(){
-
+module.exports = class Calculator {
+  constructor() {
     this.memory = [];
+  }
+  //function that returns the sum of added numbers
+  add() {
+    let sum = 0;
 
-}
-
-add(){
-
-let sum = 0;
-
-for(let i=0;i<arguments.length;i++){
-
-    sum += arguments[i];  
-
-}
-this.total = sum;
-
-return sum;
-}
-
-multiply(){
-
-let sum = arguments[0];
-
-for(let i=0;i<arguments.length;i++){
-    
-    sum *= arguments[i];  
-    
- }
- this.total = sum;
+    for (let i = 0; i < arguments.length; i++) {
+      sum += arguments[i];
+    }
+    this.total = sum;
 
     return sum;
-}
+  }
+  //function that returns the sum of multiples
+  multiply() {
+    let sum = arguments[0];
 
+    for (let i = 0; i < arguments.length; i++) {
+      sum *= arguments[i];
+    }
+    this.total = sum;
 
+    return sum;
+  }
+  //returns the last result
+  last() {
+    return this.total;
+  }
+  //sets a slot number
+  set_slot(slotNumber) {
+    this.memory.push(this.last());
 
-last(){
- return this.total;
-}
-
-set_slot(slotNumber){
-
-this.memory.push(this.last());
-  
-return this.memory[slotNumber-1];
-
-}
-
-
-get_slot(slotNumber){
-
-return this.memory[slotNumber-1];
-
-}
-
-}
-
-
+    return this.memory[slotNumber - 1];
+  }
+  //gets a slot number
+  get_slot(slotNumber) {
+    return this.memory[slotNumber - 1];
+  }
+};
 
 // let calculator = new Calculator();
 // calculator.add(1,2,3,4,5)
